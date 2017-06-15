08f37f606f2374026598e617e0be2b9368b73c10
Hiroshi Murakami <piroz@users.noreply.github.com>
2017-06-15 16:12:58 +0900

    add polyfill for old browser's

--- src/index.js
+++ src/index.js
@@ -4,6 +4,17 @@ import App from "./app";
 import "normalize.css";
 import "./index.css";
 
+import Promise from 'promise-polyfill'; 
+
+// To add to window
+if (!window.Promise) {
+  window.Promise = Promise;
+}
+
+import Find from "array.prototype.find";
+
+Find.shim();
+
 /**
  * root render
  */
