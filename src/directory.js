64a9025aaef4e89a7050bbe30e9c47a222186c12
Hiroshi Murakami <piroz@users.noreply.github.com>
2017-06-15 18:17:33 +0900

    add css-transition

--- src/directory.js
+++ src/directory.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import DirectoryEntry from "./directoryentry";
 import {Icon} from "react-fa";
+import ReactCSSTransitionGroup from "react-addons-css-transition-group";
 
 /**
  * Directory
@@ -48,13 +49,24 @@ export default class Directory extends Component {
      * @memberof Directory
      */
     render() {
+
+        let component = this.state.isOpen ? this.props.directory.entry.map((file, index) =>
+                    <DirectoryEntry key={index}  filename={file} directory={this.props.directory.dir} preview={this.props.preview}/>
+                ) : "";
+
         return (
             <div className="directory">
                 <h1 className="directory-name" onClick={this.toggleDirectory}>{this.state.isOpen ? <Icon name="folder-open-o" /> : <Icon name="folder-o" /> }{this.props.directory.dir}</h1>
                 <div className="directory-entries" style={this.state.isOpen ? {display: "block"} : {display: "none"}}>
-                {this.props.directory.entry.map((file, index) =>
-                    <DirectoryEntry key={index}  filename={file} directory={this.props.directory.dir} preview={this.props.preview}/>
-                )}
+                <ReactCSSTransitionGroup
+                    transitionName = "fade"
+                    transitionAppear = {true}
+                    transitionEnter = {true}
+                    transitionLeave = {false}
+                    transitionAppearTimeout = {500}
+                    transitionEnterTimeout = {500}>
+                    {component}
+                </ReactCSSTransitionGroup>
                 </div>
             </div>
         );
